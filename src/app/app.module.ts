6d88353b4f62a29e373c841fe0013ff901a0c671
Daniel <danielalbertorosso@gmail.com>
2021-07-15 14:10:37 -0300

    Base line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PedidoComponent } from './components/pedido/pedido.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    MenuComponent,
+    PedidoComponent,
+    ProductoComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
