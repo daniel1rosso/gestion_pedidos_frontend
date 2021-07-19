44863d495f01f976dbc12688c88b5157e1ea2003
Daniel <danielalbertorosso@gmail.com>
2021-07-19 19:43:48 -0300

    add login and icon for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import{UrlService} from 'src/app/service/url.service'
+import { Router } from '@angular/router';
+import {LoginUsuario} from 'src/app/models/login-usuario'
+import {TokenService} from 'src/app/service/token.service'
+import {LoginService} from 'src/app/service/login.service'
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isLogged = false;
+  isLoginFail = false;
+  loginUsuario: LoginUsuario;
 
-  constructor() { }
+  username: string;
+  password: string;
+
+  roles: string[] =[];
+  errMsj: string;
+
+  constructor(private tokenService : TokenService,
+    private urlService: UrlService,
+    private loginService : LoginService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onLogin(){
+
+  }
 }
